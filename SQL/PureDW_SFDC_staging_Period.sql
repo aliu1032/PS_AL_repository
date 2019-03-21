9749881680a2120b9d73546d16e2efd97117479d
aliu <april_liu@yahoo.com>
2019-03-21 15:36:22 -0700

    Initial copy

--- SQL/PureDW_SFDC_staging_Period.sql
+++
@@ -1,4 +0,0 @@
-select startDate, EndDate, FullyQualifiedLabel, [Type]
-from [PureDW_SFDC_staging].[dbo].[Period]
-where StartDate >= '2018-02-01' and EndDate <= '2020-01-31'
-and [Type] = 'Quarter'
