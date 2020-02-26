af70a9bc2795877e0df5391c038e46f69fbd787c
aliu <april_liu@yahoo.com>
2020-02-26 10:40:26 -0800

    FY21-V1

--- SE_Bookings/src/project_config.py
+++ SE_Bookings/src/project_config.py
@@ -10,13 +10,18 @@ pd.set_option('display.float_format', '{:.2f}'.format)
 
 
 
-source_data_folder = "C:\\Users\\aliu\\Box Sync\\aliu\\workspace\\SE_Performance\\"
-sup_folder = "C:\\Users\\aliu\\Box Sync\\aliu\\workspace\\SE_Performance\\"
+#source_data_folder = "C:\\Users\\aliu\\Box Sync\\aliu\\workspace\\SE_Performance\\"
+#sup_folder = "C:\\Users\\aliu\\Box Sync\\aliu\\workspace\\SE_Performance\\"
+source_data_folder = "G:\\My Drive\\Sync Laptop on Google Drive\\workspace\\SE_Performance\\"
+sup_folder = "G:\\My Drive\\Sync Laptop on Google Drive\\workspace\\SE_Performance\\"
+
+#output_folder = "C:\\Users\\aliu\\Box Sync\\aliu\\workspace\\SE_Performance\\Output_Data\\"
+output_folder = "G:\\My Drive\\Sync Laptop on Google Drive\\workspace\\SE_Performance\\Output_Data\\"
+
 
 #sql_folder = "C:\\Users\\aliu\\Box Sync\\aliu\\workspace\\SQL\\"
 sql_folder = "C:\\Users\\aliu\\git\\PS_AL_repository\\SQL\\"
 
 
-output_folder = "C:\\Users\\aliu\\Box Sync\\aliu\\workspace\\SE_Performance\\Output_Data\\"
 
 
